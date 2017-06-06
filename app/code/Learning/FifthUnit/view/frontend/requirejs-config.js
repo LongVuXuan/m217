9d5433ae79152beed06a3ef599b6460ee9bda352
longvx <longvuxuan1989@gmail.com>
2017-06-06 16:27:03 +0700

    Learning First

---
+++ app/code/Learning/FifthUnit/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by anthony on 6/6/17.
+ */
+var config = {
+    map: {
+        '*': {
+            hello: 'Learning_FifthUnit/js/hello',
+        }
+    }
+};
---
+++ app/code/Learning/FifthUnit/view/frontend/web/js/hello.js
@@ -0,0 +1,12 @@
+/**
+ * Created by anthony on 6/6/17.
+ */
+define([
+        "jquery"
+    ], function($){
+        "use strict";
+        return function(config, element) {
+            alert(config.message);
+        }
+    }
+)
